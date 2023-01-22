a00780a52cc9a09ce27425a975cec1188e348d26
LouisLangellier <louis.langellier@gmail.com>
2023-01-22 23:09:35 +0100

    Front end of home_screen done

---
+++ lib/screens/home.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 100.0),
+            child: Container(
+              height: MediaQuery.of(context).size.height / 3,
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/home.jpg'),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          const Gap(60),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 1.2,
+                child: const Center(
+                  child: Text(
+                    "Best furniture in your home",
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const Gap(20),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 1.2,
+                child: const Center(
+                  child: Text(
+                    "The best simple place where you discover most wonderful furnitures and make your home beautiful",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.grey),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const Gap(80),
+          SizedBox(
+            width: 200,
+            height: 50,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  backgroundColor: Colors.orange,
+                  textStyle: const TextStyle(
+                      fontSize: 22, fontWeight: FontWeight.w500) // foreground
+                  ),
+              onPressed: () {},
+              child: const Text('Get started'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
